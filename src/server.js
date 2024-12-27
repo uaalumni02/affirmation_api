85eb8f0897695e016611d76914af17b5c0879a65
uaalumni02 <dbell1911@gmail.com>
2024-12-27 16:17:19 -0600

    setup express server

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import "dotenv/config";
+import mongoose, { connect } from "mongoose";
+import cors from "cors";
+
+const app = express();
+const { log, error } = console;
+
+const port = process.env.PORT || 3000;
+
+const router = express.Router();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const DB_URL = process.env.MONGO_URL;
+
+try {
+  mongoose.connect(DB_URL, { useNewUrlParser: true });
+  console.log("Connection Successful");
+} catch (err) {
+  console.error("Unable to Connect to MongoDB", err);
+}
+
+app.use("/api", router);
+
+app.listen(port, () => log("server is running"));
+export default app;
